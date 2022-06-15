67e7171bd25642efc5cd4e49fc1419ea48aed8c0
FrequencyUnk <77994041+FrequencyUnk@users.noreply.github.com>
2022-06-14 22:36:23 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -47,11 +47,11 @@ resource "google_compute_instance" "hashicat" {
   name         = "${var.prefix}-hashicat"
   zone         = "${var.region}-b"
   machine_type = var.machine_type
-
+  
   tags = {
-    name = "${var.prefix}-hashicat-instance"
-    department = "devops team"
-    billable = "true"
+    Name = "${var.prefix}-hashicat-instance"
+     Department = "devops team"
+     Billable = "true"
   }
   
   boot_disk {
